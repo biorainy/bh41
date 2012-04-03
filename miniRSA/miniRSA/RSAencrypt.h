3e4326a321b9bd78cdf0c1a91abbb68c847ee7a6
baichuan <biorainy@gmail.com>
2012-04-03 16:09:55 -0400

    created server, client, and RSAencrypt files

---
+++ miniRSA/miniRSA/RSAencrypt.h
@@ -0,0 +1,14 @@
+//
+//  RSAencrypt.h
+//  miniRSA
+//
+//  Created by Baichuan Li on 4/3/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#ifndef miniRSA_RSAencrypt_h
+#define miniRSA_RSAencrypt_h
+
+
+
+#endif
---
+++ miniRSA/miniRSA/client.cpp
@@ -0,0 +1,9 @@
+//
+//  client.cpp
+//  miniRSA
+//
+//  Created by Baichuan Li on 4/3/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
